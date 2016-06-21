94ba1168a801ec23e31701bbd8a7497f29202bed
Nick <nick.park@student.manchester.ac.uk>
2016-06-21 23:10:35 +0100

    pretty much working

--- KeyboardInput/KeyboardInput.pde
+++ KeyboardInput/KeyboardInput.pde
@@ -1,19 +1,29 @@
+import java.util.HashSet;
 import processing.serial.*;
 
 private static Serial arduinoPort = null;
+private static HashSet<Integer> acceptedKeys;
 
 void setup()
 {
   // open a connection to the arduino (serial port at index 1)
   try
   {
-    arduinoPort = new Serial(this, Serial.list()[1], 250000);
+    arduinoPort = new Serial(this, Serial.list()[1], 9600);
     arduinoPort.bufferUntil('\n');
   }
   catch(ArrayIndexOutOfBoundsException e)
     { println("No serial port available at index 1"); }
   catch(NullPointerException e)
     { println("Failed to open serial port"); }
+    
+  acceptedKeys = new HashSet<Integer>();
+  acceptedKeys.add(87);
+  acceptedKeys.add(83);
+  acceptedKeys.add(65);
+  acceptedKeys.add(68);
+  acceptedKeys.add(33);
+  acceptedKeys.add(34);
 } // setup
 
 void draw()
@@ -22,5 +32,7 @@ void draw()
 
 void keyPressed()
 {
-  arduinoPort.write(key);
+  if(acceptedKeys.contains(keyCode))
+    arduinoPort.write(keyCode);
+  println("pressed " + int(key) + " " + keyCode);
 } // keyPressed
--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -1,43 +1,134 @@
-#include <StackArray.h>
 #include <Snake.h>
 #include <CubeInterface.h>
 
 Snake *snake;
+Snake *food;
 CubeInterface cube(1);
+
+const byte FORWARD = 87, BACKWARD = 83, LEFT = 65,
+           RIGHT = 68, UP = 33, DOWN = 34;
+
+byte snakeDirection;
 
 void setup()
 {
   setInitialPosition();
+  placeFood();
 } // setup
 
 void loop()
 {
   Serial.begin(9600);
   updateCube();
-  cube.wait(1000);
-  snake->xPos++;
+  cube.wait(600);
+  moveSnake();
 } // loop
+
+void moveSnake()
+{
+  Snake *head
+    = new Snake(snake->xPos, snake->yPos, snake->zPos);
+
+  switch (snakeDirection)
+  {
+    case FORWARD: head->yPos++; break;
+    case BACKWARD: head->yPos--; break;
+    case LEFT: head->xPos--; break;
+    case RIGHT: head->xPos++; break;
+    case UP: head->zPos++; break;
+    case DOWN: head->zPos--; break;
+  } // switch
+
+  if (!checkCrash(head))
+  {
+    head->next = snake;
+    snake = head;
+
+    if(snake->xPos == food->xPos && snake->yPos == food->yPos
+           && snake->zPos == food ->zPos)
+    {
+      delete food;
+      placeFood();
+    } // if
+    else
+      moveTail();
+  } // if
+  else
+  {
+    deathAnimation();
+  }
+} // move
+
+void placeFood()
+{
+  byte foodX, foodY, foodZ;
+  do
+  {
+    foodX = random(0, 8);
+    foodY = random(0, 8);
+    foodZ = random(0, 8);
+  } while(cube.ledStatus[foodX][foodY][foodZ] != LOW);
+  
+  food = new Snake(foodX, foodY, foodZ);
+}
+
+void deathAnimation()
+{
+  for (int i = 0; i < 5; i++)
+  {
+    delay(200);
+    cube.wait(400);
+  }
+} // deathAnimation
+
+boolean checkCrash(Snake *head)
+{
+  return (head->xPos < 0) || (head->xPos > 7)
+         || (head->yPos < 0) || (head->yPos > 7)
+         || (head->zPos < 0) || (head->zPos > 7);
+}
+
+void moveTail()
+{
+  Snake *tail = snake;
+  while (tail->next->next != NULL)
+  {
+    tail = tail->next;
+  }
+  delete tail->next;
+  tail->next = NULL;
+}
 
 void updateCube()
 {
-  Snake *body = snake;
+  cube.clearAll();
 
-  while(body != NULL)
+  Snake *snakeBody = snake;
+
+  while (snakeBody != NULL)
   {
-    cube.light(body->xPos, body->yPos, body->zPos);
-    body = body->next;
+    cube.light(snakeBody->xPos, snakeBody->yPos,
+               snakeBody->zPos);
+    snakeBody = snakeBody->next;
   }
+
+  cube.light(food->xPos, food->yPos, food->zPos);
 }
 
 void setInitialPosition()
 {
+  snakeDirection = FORWARD;
   snake = new Snake(0, 3, 0);
-  
+
   Snake *body = snake;
-  for(int i=2; i >= 0; i--)
+  for (int i = 2; i >= 0; i--)
   {
     body->next = new Snake(0, i, 0);
     body = body->next;
   }
 } // setInitialPosition
 
+void serialEvent()
+{
+  snakeDirection = (byte)Serial.read();
+} // serialEvent
